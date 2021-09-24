6e6566045b69e24e9ac30a3f0b2560d767ddaa15
Caio042 <caiohenriquegomeslima@gmail.com>
2021-09-23 23:37:41 -0300

    Criação do programa 'ola-e-versao-do-sistema'

---
+++ variaveis/ola-e-versao-do-sistema.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Caio"
+	var versao float32 = 0.9
+	fmt.Println("Olá,", nome)
+	fmt.Println("Você está utilizando o programa na versão", versao)
+}
