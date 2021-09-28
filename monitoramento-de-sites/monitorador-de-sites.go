efd12d1cb7dae2ebb14be1e00cb2e1ba1f0992eb
Caio042 <caiohenriquegomeslima@gmail.com>
2021-09-27 21:59:51 -0300

    Refatoração, extraindo funções

--- monitoramento-de-sites/monitorador-de-sites.go
+++ monitoramento-de-sites/monitorador-de-sites.go
@@ -1,18 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("1 - Monitorar sites")
-	fmt.Println("2 - Logs")
-	fmt.Println("\n0 - Sair")
-	fmt.Println("\nDigite a opção desejada:")
+	exibeMenu()
 
-	var escolha int
-	// recebe o valor digitado e armazena na variavel, recebe como parametro o ponteiro da variavel onde será armazenada, indicado com o &
-	fmt.Scan(&escolha)
-
+	escolha := leComando()
 	//ifs sem parenteses
 	if escolha == 1 {
 		fmt.Println("Monitorando... ")
@@ -23,4 +20,33 @@ func main() {
 	} else {
 		fmt.Println("Escolha inválida")
 	}
+
+	switch escolha{
+	case 0:
+		sair()
+	default:
+		os.Exit(-1)
+	}
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Monitorar sites")
+	fmt.Println("2 - Logs")
+	fmt.Println("\n0 - Sair")
+	fmt.Println("\nDigite a opção desejada:")
+}
+
+func leComando() int {
+	var escolha int
+	// recebe o valor digitado e armazena na variavel, recebe como parametro o ponteiro da variavel onde será armazenada, indicado com o &
+	fmt.Scan(&escolha)
+	return escolha
+}
+
+func monitorarSites() {
+
+}
+
+func sair() {
+	os.Exit(0)
 }
